805d3af6d1a5e549ed053192addd83df9b396574
Ganesh Shinde <gshinde1090@gmail.com>
2020-12-15 01:39:21 +0530

    Added network library classes.

--- Sources/NetworkManager/NetworkManager.swift
+++ Sources/NetworkManager/NetworkManager.swift
@@ -1,3 +1,24 @@
-struct NetworkManager {
-    var text = "Hello, World!"
+
+import Network
+import Foundation
+
+@available(iOS 13.0, *)
+final class NetworkManager: ObservableObject {
+    
+    let monitor = NWPathMonitor()
+    let queue = DispatchQueue(label: "networkmanager.queue")
+    
+    /// Update connenction status
+    @Published var isConnected = true
+    
+    
+    init() {
+        monitor.pathUpdateHandler = { [weak self] path in
+            DispatchQueue.main.async {
+                self?.isConnected = path.status == .satisfied ? true : false
+            }
+        }
+        
+        monitor.start(queue: queue)
+    }
 }
--- Tests/NetworkManagerTests/NetworkManagerTests.swift
+++ Tests/NetworkManagerTests/NetworkManagerTests.swift
@@ -2,11 +2,12 @@ import XCTest
 @testable import NetworkManager
 
 final class NetworkManagerTests: XCTestCase {
+    
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(NetworkManager().text, "Hello, World!")
+        XCTAssertEqual(NetworkManager().isConnected, true)
     }
 
     static var allTests = [
